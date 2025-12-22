39ba778c485c7b9f784684bde639c934114ee5fa
Esteban Vargas Quirós <79805608+estebanvquiros@users.noreply.github.com>
2025-12-22 17:57:48 -0600

    Add highlight to active nav button
    Implements a highlight on the active navbar button to indicate the
    currently displayed page.

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ const content = document.querySelector(".content");
 const homeButton = document.querySelector("#homeButton");
 const menuButton = document.querySelector("#menuButton");
 const aboutButton = document.querySelector("#aboutButton");
+const navButtons = document.querySelectorAll(".nav-button");
 
 loadHomePage();
 
@@ -17,19 +18,30 @@ aboutButton.addEventListener("click", renderAboutPage);
 
 function renderHomePage() {
 	clearContent();
+	updateActiveButton(homeButton);
 	loadHomePage();
 }
 
 function renderMenu() {
 	clearContent();
+	updateActiveButton(menuButton);
 	loadMenu();
 }
 
 function renderAboutPage() {
 	clearContent();
+	updateActiveButton(aboutButton);
 	loadAboutPage();
 }
 
 function clearContent() {
 	content.replaceChildren();
+}
+
+function updateActiveButton(button) {
+	console.log(navButtons);
+	navButtons.forEach((btn) => {
+		btn.classList.remove("active");
+	});
+	button.classList.add("active");
 }
